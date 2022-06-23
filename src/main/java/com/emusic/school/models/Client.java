b538b75c104584a42b4eaaf520b805dab98c1b74
AlejandroRaulRodriguez <my_email>
2022-06-23 12:20:12 -0300

    clase client

--- src/main/java/com/emusic/school/models/Client.java
+++ src/main/java/com/emusic/school/models/Client.java
@@ -1,4 +1,48 @@
 package com.emusic.school.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean isActive;
+
+    public Client() {}
+
+    public Client(String firstName, String lastName, String email, String password, boolean isActive) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.isActive = isActive;
+    }
+
+    public long getId() {return id;}
+
+    public String getFirstName() {return firstName;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
+    public boolean isActive() {return isActive;}
+    public void setActive(boolean active) {isActive = active;}
 }
