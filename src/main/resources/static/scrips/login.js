7d958fd6626e031df16764e47e516d195fdbe836
romanjbenitez <roman200217@gmail.com>
2022-06-27 12:32:01 -0300

    funcionalidad de login

--- src/main/resources/static/scrips/login.js
+++ src/main/resources/static/scrips/login.js
@@ -1,10 +1,40 @@
-let container = document.getElementById('login-container')
+Vue.createApp({
+	data() {
+	return {
+	container : null,
+	email : "",
+	password : "",
 
-toggle = () => {
-	container.classList.toggle('sign-in')
-	container.classList.toggle('sign-up')
-}
+	}
+	},
 
-setTimeout(() => {
-	container.classList.add('sign-in')
-}, 200)
+	created() {
+	},
+	
+	mounted(){
+	this.$nextTick(function () {
+	this.container = document.getElementById('login-container')
+	})
+	setTimeout(() => {
+		this.container.classList.add('sign-in')
+		}, 200)
+	},
+	methods: {
+		toggle(){
+			this.container.classList.toggle('sign-in')
+			this.container.classList.toggle('sign-up')
+		},
+		signUp(){
+		axios.post(
+            "/api/login",
+            `email=${this.email}&password=${this.password}`,
+            { headers: { "content-type": "application/x-www-form-urlencoded" } }).then((res) => {
+				window.location.replace("./students.html")
+			})
+		}
+	},
+	computed: {
+		
+	}
+	},
+).mount("#app")
